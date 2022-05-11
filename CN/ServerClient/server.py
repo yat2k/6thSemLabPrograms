c22429eea0d1e06e723c414ae98c638f5d24e6a5
Yat <63180414+yat2k@users.noreply.github.com>
2022-05-11 10:27:43 +0530

    Create server.py

---
+++ CN/ServerClient/server.py
@@ -0,0 +1,25 @@
+import socket
+
+s = socket.socket()
+print("Socket successfully created\n")
+
+port = 6000
+s.bind(('',port))
+print("Socket binded to %s"%(port))
+
+s.listen(5)
+print("Socket is Listening")
+
+while 1:
+
+        c,addr = s.accept()
+
+        print("Got Connection from",addr)
+
+        c.send("Succesfully Connected".encode())
+
+        msg = c.recv(1024).decode().lower()
+        c.send(msg)
+        c.close()
+
+        break
