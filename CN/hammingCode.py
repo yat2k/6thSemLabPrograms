0a9ac889a1846b5e6da76f9c4485eb7319288333
Yat <63180414+yat2k@users.noreply.github.com>
2022-05-17 20:14:36 +0530

    Update hammingCode.py

--- CN/hammingCode.py
+++ CN/hammingCode.py
@@ -45,11 +45,19 @@ print("The syndrome is", (actuSyn))
 
 msg = "".join(codeWord)
 
-if int(actuSyn) == 0: print("No Error In The Code")
-elif int(actuSyn) == 1: print("Error in Q0\nThe corrected message is",msg);
-elif int(actuSyn) == 10: print("Error in Q1\nThe corrected message is",msg);
-elif int(actuSyn) == 11: print("Error in B2\nThe corrected message is",msg);
-elif int(actuSyn) == 100: print("Error in Q2\nThe corrected message is",msg);
-elif int(actuSyn) == 101: print("Error in B0\nThe corrected message is",msg);
-elif int(actuSyn) == 110: print("Error in B3\nThe corrected message is",msg);
-elif int(actuSyn) == 111: print("Error in Q0\nThe corrected message is",msg);
+if int(actuSyn) == 0:
+    print("No Error In The Code")
+elif int(actuSyn) == 1:
+    print("Error in Q0\nThe corrected message is",msg);
+elif int(actuSyn) == 10:
+    print("Error in Q1\nThe corrected message is",msg);
+elif int(actuSyn) == 11:
+    print("Error in B2\nThe corrected message is",msg);
+elif int(actuSyn) == 100:
+    print("Error in Q2\nThe corrected message is",msg);
+elif int(actuSyn) == 101:
+    print("Error in B0\nThe corrected message is",msg);
+elif int(actuSyn) == 110:
+    print("Error in B3\nThe corrected message is",msg);
+elif int(actuSyn) == 111:
+    print("Error in Q0\nThe corrected message is",msg);
