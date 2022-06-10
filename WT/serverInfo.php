fa37bef81a337ea647ee4f59294b3ed9e2106dc3
Yathin S Rao <63180414+yat2k@users.noreply.github.com>
2022-06-10 11:31:38 +0530

    Create serverInfo.php

---
+++ WT/serverInfo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['SERVER_SOFTWARE'];
+echo "<br>";
+echo $_SERVER['SERVER_PROTOCOL'];
+echo "<br>";
+echo $_SERVER['SERVER_ADDR'];
+echo "<br>";
+echo $_SERVER['GATEWAY_INTERFACE'];
+echo "<br>";
+echo $_SERVER['REQUEST_METHOD'];
+echo "<br>";
+echo $_SERVER['SERVER_PORT'];
+?>
+
+</body>
+</html>
