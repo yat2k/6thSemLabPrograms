1b5e7f276fe6e217d7bf25cdea35221baf00d806
Yathin S Rao <63180414+yat2k@users.noreply.github.com>
2022-06-10 12:26:42 +0530

    Create largest.php

---
+++ WT/php/largest.php
@@ -0,0 +1,7 @@
+<?php
+$a=(int)readline('Enter the first number: ');
+$b=(int)readline('Enter the first number: ');
+$c=(int)readline('Enter the first number: ');
+$large = max($a,$b,$c);
+echo $large;
+?>
