4e7af29a56d931e19a60b868431c2a2394e80500
Yathin S Rao <63180414+yat2k@users.noreply.github.com>
2022-07-01 12:49:22 +0530

    Update DBConnection.php

--- WT/php/library/DBConnection.php
+++ WT/php/library/DBConnection.php
@@ -3,6 +3,6 @@
 define('DB_SERVER', 'localhost:3306');
 define('DB_USERNAME', 'root');   // username:- viis{number}
 define('DB_PASSWORD', 'root123'); // password:-^^^
-define('DB_DATABASE', 'books'); //where books is the database name  ---> make this the db of your choice 
+define('DB_DATABASE', 'test'); //where books is the database name  ---> make this the db of your choice 
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
