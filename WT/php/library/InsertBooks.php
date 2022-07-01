26bab370de7e66be1670b498d820026ba9049d89
Yathin S Rao <63180414+yat2k@users.noreply.github.com>
2022-07-01 12:33:35 +0530

    Create InsertBooks.php

---
+++ WT/php/library/InsertBooks.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<body bgcolor="87ceeb">
+    <center><h2>Simple Library Management System</h2></center>
+    <br>
+ 
+    <?php
+        include("DBConnection.php");
+ 
+        $isbn=$_POST["isbn"];
+        $title=$_POST["title"];
+        $author=$_POST["author"];
+        $edition=$_POST["edition"];
+        $publication=$_POST["publication"];
+ 
+        $query = "insert into book_info(isbn,title,author,edition,publication) values('$isbn','$title','$author','$edition','$publication')"; //Insert query to add book details into the book_info table
+        $result = mysqli_query($db,$query);
+    ?>
+    <h3> Book information is inserted successfully </h3>
+ 
+    <a href="SearchBooks.php"> To search for the Book information click here </a>
+ 
+</body>
+</html>
