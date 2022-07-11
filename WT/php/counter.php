8a74a2804d6b737a8b6ae40f9f7a8cea3f42b265
Yathin S Rao <63180414+yat2k@users.noreply.github.com>
2022-07-11 12:33:02 +0530

    Update counter.php

--- WT/php/counter.php
+++ WT/php/counter.php
@@ -12,3 +12,19 @@ fwrite($file, $hits[0]);
 
 echo "Total number of views: ".$hits[0];
 ?>
+
+
+another code if the above one doesnt work in putty SSH
+
+<?php
+ 
+$count_page = ("counter.txt");
+$hits = file($count_page);
+$hits[0] ++;
+ 
+$fp = fopen($count_page , "w");
+fputs($fp , "$hits[0]");
+fclose($fp);
+echo $hits[0];
+ 
+?>
