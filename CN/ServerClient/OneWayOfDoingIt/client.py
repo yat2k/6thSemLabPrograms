e361a44fe0f4a0f641deabd187e0bd4ef31d58a6
Yathin S Rao <63180414+yat2k@users.noreply.github.com>
2022-07-14 00:56:55 +0530

    Update client.py

--- CN/ServerClient/OneWayOfDoingIt/client.py
+++ CN/ServerClient/OneWayOfDoingIt/client.py
@@ -9,3 +9,22 @@ msg=input("Enter the text to be Conveted : ")
 s.send(msg.encode())
 print(s.recv(1024).decode())
 s.close()
+
+
+
+#for python 2 use the below code (literally the same with raw_input)
+
+import socket
+
+s=socket.socket()
+
+port = 6000
+
+s.connect(('127.0.0.1',port))
+print(s.recv(1024).decode())
+
+msg  =raw_input("Enter the text to be Conveted : ")
+s.send(msg)
+print(s.recv(1024).decode())
+
+s.close()
